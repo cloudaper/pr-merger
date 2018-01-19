dc23cafe89fcc4222137e199f1c405ee6fb34234
Josef Strzibny <strzibny@strzibny.name>
2018-01-19 16:55:32 +0100

    Introduce skip_pr?

--- lib/gh_pr_merger.rb
+++ lib/gh_pr_merger.rb
@@ -1,22 +1,27 @@
 require 'gh_pr_merger/version'
-require 'octokit'
 require 'tty-command'
+require 'octokit'
 
-module GhPrMerger
+Octokit.auto_paginate = true
 
-  APP_CONTEXT = 'ci/gh_pr_merger'
+class GhPrMerger
+  APP_CONTEXT    = 'ci/gh_pr_merger'
+  AUTOMERGE_SKIP = '[automerge skip]'
 
   def self.run(base_repo, access_token, base_branch, merge_branch, fork_repo)
-    cmd = TTY::Command.new
-    Octokit.auto_paginate = true
-    client = Octokit::Client.new(access_token: access_token)
+    @base_repo = base_repo
+    @client = Octokit::Client.new(access_token: access_token)
     pull_requests = client.pull_requests(base_repo, state: 'open').reverse
+
+    cmd = TTY::Command.new
     active_branch_result = cmd.run! 'git rev-parse --abbrev-ref HEAD'
+
     if active_branch_result.failure?
       previous_branch = cmd.run('cat .git/HEAD').strip
     else
       previous_branch = active_branch_result.out.strip
     end
+
     cmd.run 'git checkout', base_branch
 
     if fork_repo
@@ -26,40 +31,66 @@ module GhPrMerger
     else
       cmd.run 'git reset --hard', "origin/#{base_branch}"
     end
+
     cmd.run 'git checkout -b', merge_branch
+
     merge_statuses = pull_requests.map do |pr|
-      process_pr(pr, client, cmd, base_repo, base_branch, fork_repo)
+      process_pr(pr, cmd, base_repo, base_branch, fork_repo)
     end
+
     cmd.run 'git checkout', previous_branch
+
     merge_statuses.all? { |status| status }
   end
 
   private
 
-  def self.process_pr(pr, client, cmd, base_repo, base_branch, fork_repo)
+  # Process a given pull request
+  def process_pr(pr, cmd, base_repo, base_branch, fork_repo)
     head = pr[:head]
     repo = head[:repo]
+
     puts "Attempting to merge #{head[:ref]}."
-    client.create_status(base_repo, head[:sha], 'pending', context: APP_CONTEXT, description: 'Merge in progress.')
-    if pr[:title].include?('[automerge skip]')
-      puts "Skipping #{head[:ref]} because of [automerge skip]."
-      client.create_status(base_repo, head[:sha], 'failure', context: APP_CONTEXT, description: 'Merge skipped because of [automerge skip].')
-      return true
-    end
+
+    @client.create_status(base_repo, head[:sha], 'pending', context: APP_CONTEXT,
+                                                           description: 'Merge in progress.')
+
+    return true if skip_pr?(pr)
+
     begin
-      cmd.run "git fetch", repo[:ssh_url], head[:ref] if repo
+      cmd.run 'git fetch', repo[:ssh_url], head[:ref] if repo
+
       merge_status = cmd.run! 'git merge --no-ff --no-edit', head[:sha]
+
       if merge_status.failure?
         cmd.run 'git merge --abort'
-        client.create_status(base_repo, head[:sha], 'failure', context: APP_CONTEXT, description: "Failed to merge '#{head[:ref]} with #{base_branch}. Check for merge conflicts.")
+
+        @client.create_status(base_repo, head[:sha], 'failure', context: APP_CONTEXT,
+                                                                description: "Failed to merge '#{head[:ref]} with #{base_branch}. Check for merge conflicts.")
       else
-        client.create_status(base_repo, head[:sha], 'success', context: APP_CONTEXT, description: "Merge with '#{base_branch}' was successful.")
+        @client.create_status(base_repo, head[:sha], 'success', context: APP_CONTEXT,
+                                                                description: "Merge with '#{base_branch}' was successful.")
       end
     rescue => e
       p e
-      client.create_status(base_repo, head[:sha], 'error', context: APP_CONTEXT, description: "Merge encountered an error: #{e.class.name}.")
+      @client.create_status(base_repo, head[:sha], 'error', context: APP_CONTEXT,
+                                                            description: "Merge encountered an error: #{e.class.name}.")
       return false
     end
-    return true
+
+    true
+  end
+
+  # Skip a given pull request if it includes automerge skip message
+  def skip_pr?(pr)
+    if pr[:title].include?(AUTOMERGE_SKIP)
+      message "Skipping #{pr[:head][:ref]} because of #{AUTOMERGE_SKIP}."
+      puts message
+      @client.create_status(@base_repo, pr[:head][:sha], 'failure', context: APP_CONTEXT, description: message)
+
+      true
+    else
+      false
+    end
   end
 end
