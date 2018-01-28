6dd6c4745486af07e8be5c4248631ebff0a19ce5
Josef Strzibny <strzibny@strzibny.name>
2018-01-28 16:17:40 +0100

    Accept params as options

--- lib/gh_pr_merger.rb
+++ lib/gh_pr_merger.rb
@@ -5,116 +5,118 @@ require 'octokit'
 Octokit.auto_paginate = true
 
 # Merges all opened GitHub PRs to a new branch
-class GhPrMerger
-  APP_CONTEXT    = 'ci/gh_pr_merger'.freeze
-  AUTOMERGE_SKIP = '[automerge skip]'.freeze
+module GhPrMerger
+  class Merger
+    APP_CONTEXT    = 'ci/gh_pr_merger'.freeze
+    AUTOMERGE_SKIP = '[automerge skip]'.freeze
 
-  def initialize(access_token:)
-    @gh = Octokit::Client.new(access_token: access_token)
-  end
-
-  def run(base_repo:, base_branch:, merge_branch:, fork_repo:)
-    @base_repo   = base_repo
-    @base_branch = base_branch
-
-    pull_requests = @gh.pull_requests(@base_repo, state: 'open').reverse
-
-    cmd = TTY::Command.new
-    active_branch_result = cmd.run! 'git rev-parse --abbrev-ref HEAD'
-
-    if active_branch_result.success?
-      previous_branch = active_branch_result.out.strip
-    else
-      previous_branch = cmd.run('cat .git/HEAD').strip
+    def initialize(access_token:)
+      @gh = Octokit::Client.new(access_token: access_token)
     end
 
-    cmd.run 'git checkout', @base_branch
+    def run(base_repo:, base_branch:, merge_branch:, fork_repo:)
+      @base_repo   = base_repo
+      @base_branch = base_branch
 
-    if fork_repo
-      cmd.run! 'git remote add upstream', "git@github.com:#{@base_repo}.git"
-      cmd.run 'git fetch upstream', @base_branch
-      cmd.run 'git reset --hard', "upstream/#{@base_branch}"
-    else
-      cmd.run 'git reset --hard', "origin/#{@base_branch}"
-    end
+      pull_requests = @gh.pull_requests(@base_repo, state: 'open').reverse
 
-    cmd.run 'git checkout -b', merge_branch
+      cmd = TTY::Command.new
+      active_branch_result = cmd.run! 'git rev-parse --abbrev-ref HEAD'
 
-    merge_statuses = pull_requests.map do |pr|
-      process_pr(pr, cmd)
-    end
-
-    cmd.run 'git checkout', previous_branch
-
-    merge_statuses.all? { |status| status }
-  end
-
-  private
-
-  # Process a given pull request
-  def process_pr(pr, cmd)
-    head = pr[:head]
-    repo = head[:repo]
-
-    pending_status(pr, 'Merge in progress.')
-
-    return true if skip_pr?(pr)
-
-    begin
-      cmd.run 'git fetch', repo[:ssh_url], head[:ref] if repo
-
-      merge_status = cmd.run! 'git merge --no-ff --no-edit', head[:sha]
-
-      if merge_status.success?
-        success_status(pr, "Merge with '#{base_branch}' was successful.")
+      if active_branch_result.success?
+        previous_branch = active_branch_result.out.strip
       else
-        cmd.run 'git merge --abort'
-
-        message = "Failed to merge '#{head[:ref]} with #{@base_branch}."
-        failure_status(pr, message)
+        previous_branch = cmd.run('cat .git/HEAD').strip
       end
-    rescue => e
-      failure_status(pr, "Merge encountered an error: #{e.class.name}.")
 
-      return false
+      cmd.run 'git checkout', @base_branch
+
+      if fork_repo
+        cmd.run! 'git remote add upstream', "git@github.com:#{@base_repo}.git"
+        cmd.run 'git fetch upstream', @base_branch
+        cmd.run 'git reset --hard', "upstream/#{@base_branch}"
+      else
+        cmd.run 'git reset --hard', "origin/#{@base_branch}"
+      end
+
+      cmd.run 'git checkout -b', merge_branch
+
+      merge_statuses = pull_requests.map do |pr|
+        process_pr(pr, cmd)
+      end
+
+      cmd.run 'git checkout', previous_branch
+
+      merge_statuses.all? { |status| status }
     end
 
-    true
-  end
+    private
 
-  # Skip a given pull request if it includes automerge skip message
-  def skip_pr?(pr)
-    return false unless pr[:title].include?(AUTOMERGE_SKIP)
+    # Process a given pull request
+    def process_pr(pr, cmd)
+      head = pr[:head]
+      repo = head[:repo]
 
-    failure_status(pr, "Skipping #{pr[:head][:ref]}.")
+      pending_status(pr, 'Merge in progress.')
 
-    true
-  end
+      return true if skip_pr?(pr)
 
-  def pending_status(pr, message)
-    send_status(pr, 'pending', message)
-  end
+      begin
+        cmd.run 'git fetch', repo[:ssh_url], head[:ref] if repo
 
-  def error_status(pr, message)
-    send_status(pr, 'error', message)
-  end
+        merge_status = cmd.run! 'git merge --no-ff --no-edit', head[:sha]
 
-  def failure_status(pr, message)
-    send_status(pr, 'failure', message)
-  end
+        if merge_status.success?
+          success_status(pr, "Merge with '#{base_branch}' was successful.")
+        else
+          cmd.run 'git merge --abort'
 
-  def success_status(pr, message)
-    send_status(pr, 'success', message)
-  end
+          message = "Failed to merge '#{head[:ref]} with #{@base_branch}."
+          failure_status(pr, message)
+        end
+      rescue => e
+        failure_status(pr, "Merge encountered an error: #{e.class.name}.")
 
-  # Send Octokit status
-  def send_status(pr, status, message)
-    @gh.create_status(
-      @base_repo,
-      pr[:head][:sha],
-      status,
-      context: APP_CONTEXT,
-      description: message
-    )
+        return false
+      end
+
+      true
+    end
+
+    # Skip a given pull request if it includes automerge skip message
+    def skip_pr?(pr)
+      return false unless pr[:title].include?(AUTOMERGE_SKIP)
+
+      failure_status(pr, "Skipping #{pr[:head][:ref]}.")
+
+      true
+    end
+
+    def pending_status(pr, message)
+      send_status(pr, 'pending', message)
+    end
+
+    def error_status(pr, message)
+      send_status(pr, 'error', message)
+    end
+
+    def failure_status(pr, message)
+      send_status(pr, 'failure', message)
+    end
+
+    def success_status(pr, message)
+      send_status(pr, 'success', message)
+    end
+
+    # Send Octokit status
+    def send_status(pr, status, message)
+      @gh.create_status(
+        @base_repo,
+        pr[:head][:sha],
+        status,
+        context: APP_CONTEXT,
+        description: message
+      )
+    end
   end
 end
